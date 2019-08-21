a8395264a2a1b23ba5ce0e7331b029f31ce611d3
Montasir007 <montasir.rana@gmail.com>
2019-08-21 19:16:22 +0600

    successful register php file

---
+++ successfulRegister.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<a class="logo" href="home.php"><img src="./logo.jpg"></a>
+
+<div class="header">
+<ul>
+	<li class="title"><a href="home.php"><span class="title">Ticket Management System</span></a></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	
+	<li><a href="signup.php"><span class="tab">Sign Up</span></a></li>
+	<li><a href="login.php"><span class="tab">Log In</span></a></li>
+</ul>
+<br><br><br>
+</div>
+
+<div class="container">
+	You Have Signed Up Succesfully!!! Now You Can Log In.			
+	
+</div>
+
+</body>
+</html>
