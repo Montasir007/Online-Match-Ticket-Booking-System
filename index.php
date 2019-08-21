8793b15cb7493915b55cd0ce07f26be548110f66
Montasir007 <montasir.rana@gmail.com>
2019-08-21 19:08:38 +0600

    index php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BPL-Ticket Management System</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+<div class="header">
+	<a href="home.php"><button class="b1">Enter into <br>BangladeshPremierLeague</button></a>
+</div>
+</body>
+</html>
